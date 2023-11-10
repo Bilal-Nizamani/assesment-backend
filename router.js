b1b3c25ef73ef269974ba3a479a887023f3b7165
Bilal-Nizamani <bilalnizamani001@gmail.com>
2023-11-10 11:37:09 +0500

    added paymentmethod

--- router.js
+++ router.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import dotenv from "dotenv";
 import axios from "axios";
+import stripe from "stripe";
+dotenv.config();
+
 const router = Router();
-
 const apiUrl = process.env.API;
-
 router.get("/api/cards", async (req, res) => {
   try {
     // Extract the page parameter from the request
@@ -16,7 +18,6 @@ router.get("/api/cards", async (req, res) => {
         pageSize: 20,
       },
     });
-
     // Extract the cards from the API response
     const cards = response.data.cards;
     // Send the cards as the API response
@@ -25,6 +26,49 @@ router.get("/api/cards", async (req, res) => {
     // Handle errors
     console.error("Error fetching cards:", error);
     res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+const stripeSecretKey = process.env.SECRET_KEY;
+const stripeInstance = new stripe(stripeSecretKey);
+router.post("/payment-checkout", async (req, res) => {
+  try {
+    const { item } = req.body;
+    const redirectURL =
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000"
+        : "https://production-url.com"; // Replace with your production URL
+
+    const transformedItem = {
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              images: [item.image],
+              name: item.name,
+            },
+            unit_amount: item.price * 100,
+          },
+          quantity: item.quantity,
+        },
+      ],
+      mode: "payment",
+      success_url: redirectURL + "?status=success",
+      cancel_url: redirectURL + "?status=cancel",
+      metadata: {
+        images: item.image,
+      },
+    };
+
+    const session = await stripeInstance.checkout.sessions.create(
+      transformedItem
+    );
+
+    res.json({ id: session.id });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to create session" });
   }
 });
 
